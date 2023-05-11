a873079d37f46680a7d8ee01a1b6c15d84ec6886
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-05-11 18:18:43 -0300

    Included access to weather and geocode APIs.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,14 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout( () => {
-    console.log('2 Second Timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=<KEY>&query=37.8267,-122.4233&units=f'
 
-setTimeout( () => {
-    console.log('0 Second Timer')
-}, 0)
+request( { url: url, json: true }, (error, response) => {
+    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature
+        + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out')
+})
 
-console.log('Stopping')
+const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=<TOKEN>'
+
+request( { url: url2, json: true }, (error, response) => {
+    console.log('Lat: ' + response.body.features[0].center[1] + ' | Long: ' + response.body.features[0].center[0])
+})
