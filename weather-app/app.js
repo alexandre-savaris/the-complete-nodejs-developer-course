1e9f452ab75f959323de93f7fd1613a06ea45c8a
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-05-14 15:12:23 -0300

    Added asynchronous code.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,22 @@
-const request = require('request')
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
 
-const url = 'http://api.weatherstack.com/current?access_key=<KEY>&query=37.8267,-122.4233&units=f'
+if (process.argv.length !== 3) {
+    return console.log('No argument was provided!')
+}
 
-request( { url: url, json: true }, (error, response) => {
-    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature
-        + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out')
-})
+geocode(process.argv[2], (error, { latitude, longitude, location } = {}) => {
 
-const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=<TOKEN>'
+    if (error) {
+        return console.log(error)
+    }
 
-request( { url: url2, json: true }, (error, response) => {
-    console.log('Lat: ' + response.body.features[0].center[1] + ' | Long: ' + response.body.features[0].center[0])
+    forecast(latitude, longitude, (error, forecastData) => {
+
+        if (error) {
+            return console.log(error)
+        }
+        console.log(location)
+        console.log(forecastData)
+    })
 })
