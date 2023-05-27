15fddae33148f90c8ffa82a5dba38726818c382f
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-05-27 17:50:55 -0300

    Started the task-manager application.

---
+++ task-manager/mongodb.js
@@ -0,0 +1,66 @@
+const { MongoClient, ObjectID } = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    const db = client.db(databaseName)
+
+    // db.collection('users').insertOne({
+    //     name: 'Vikram',
+    //     age: 26
+    // }, (error, result) => {
+
+    //     if (error) {
+    //         return console.log('Unable to insert user!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+
+    //     if (error) {
+    //         console.log('Unable to insert users!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Task 1',
+    //         completed: true
+    //     }, {
+    //         description: 'Task 2',
+    //         completed: false
+    //     }, {
+    //         description: 'Task 3',
+    //         completed: true
+    //     }
+    // ], (error, result) => {
+
+    //     if (error) {
+    //         console.log('Unable to insert tasks!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+})
