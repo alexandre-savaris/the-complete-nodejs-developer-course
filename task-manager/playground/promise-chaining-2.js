ec04c679dcd775f738331d2b6a3265588dbd4cb6
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-06-03 18:06:46 -0300

    Added async/await.

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,24 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('64766567440de10b2ca14a94').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('64766567440de10b2ca14a94').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('647911c342c8811a607d844e').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
