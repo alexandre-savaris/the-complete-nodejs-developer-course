f0ba66f434a796e0e2538cbbcfef677722466fd1
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-06-08 19:56:13 -0300

    Added JWT, express middleware.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,6 +8,20 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled!')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+
+//     res.status(503).send('The site is on maintenance!')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -16,18 +30,15 @@ app.listen(port, () => {
     console.log('Server is up and running!')
 })
 
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const myFunction = async () => {
 
-    const password = 'Red12345!'
-    const hashedPassword = await bcrypt.hash(password, 8)
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
+    console.log(token)
 
-    console.log(password)
-    console.log(hashedPassword)
-
-    const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
-    console.log(isMatch)
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
 }
 
 myFunction()
