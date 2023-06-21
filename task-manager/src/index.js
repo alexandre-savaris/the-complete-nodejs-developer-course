195ef66de6b7732b91701e8862040b8287528ef4
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-06-21 18:44:18 -0300

    Added support for file uploads.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,19 +8,26 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
 
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled!')
-//     } else {
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
+const multer = require('multer')
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document!'))
+        }
+        cb(undefined, true)
+    }
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
 
-//     res.status(503).send('The site is on maintenance!')
-// })
+
 
 app.use(express.json())
 app.use(userRouter)
@@ -29,16 +36,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up and running!')
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-}
-
-myFunction()
