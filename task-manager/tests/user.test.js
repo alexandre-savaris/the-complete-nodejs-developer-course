2e84a79b48f6c032dd08dda7509222c9a38354ce
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-06-29 19:06:57 -0300

    Added test cases.

---
+++ task-manager/tests/user.test.js
@@ -0,0 +1,114 @@
+const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+    _id: userOneId,
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '1234567890',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
+}
+
+beforeEach(async () => {
+
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+test('Should signup a new user', async () => {
+
+    const response = await request(app)
+        .post('/users')
+        .send({
+            name: 'Alexandre Savaris',
+            email: 'alexandre.savaris@gmail.com',
+            password: '1234567890'
+        })
+        .expect(201)
+
+    // Asserts that the database was changed correctly.
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    // Assertions about the response.
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Alexandre Savaris',
+            email: 'alexandre.savaris@gmail.com'
+        },
+        token: user.tokens[0].token
+    })
+
+    // Asserts that the password is not stored in plain text.
+    expect(user.password).not.toBe('1234567890')
+})
+
+test('Should login existing user', async () => {
+
+    const response = await request(app)
+        .post('/users/login')
+        .send({
+            email: userOne.email,
+            password: userOne.password
+        })
+        .expect(200)
+
+    // Asserts that token in response matches users second token.
+    const user = await User.findById(userOne._id)
+    expect(response.body.token).toBe(user.tokens[1].token)
+})
+
+test('Should not login nonexistent user', async () => {
+
+    await request(app)
+        .post('/users/login')
+        .send({
+            email: 'xxx@yyy.zz',
+            password: '9988776655'
+        })
+        .expect(400)
+})
+
+test('Should get profile for user', async () => {
+
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+test('Should delete account for user', async () => {
+
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    // Asserts that the user was removed.
+    const user = await User.findById(userOne._id)
+    expect(user).toBeNull()
+})
+
+test('Should not delete account for unauthenticated user', async () => {
+
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
+})
